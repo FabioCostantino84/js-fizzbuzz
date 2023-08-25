f1dd8866486a9003950e2be2652d4db49e123244
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-25 16:37:26 +0200

    add consol multipli

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,23 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 */
 
 //stampare in console i numeri da 1 a 100
-let element = document.getElementById('list');
-
-for (let i = 1; i <= 100; i++){
-    console.log(i);
-   
-}
 //stampare in console per i multipli di 3 "Fizz"
 //stampare in console per i multipli di 3 "Fizz"
 //stampare in console sia per i multipli di 3 e di 5 "FizzBuzz"
+
+for (let i = 1; i <= 100; i++) {
+
+    if ((i % 3) === 0 && (i % 5) === 0) {
+        console.log("FizzBuzz");
+       
+    } else if ((i % 3) === 0) {
+        console.log("Fizz");
+       
+    } else if ((i % 5) === 0) {
+        console.log("Buzz");
+       
+    } else {
+        console.log(i);
+      
+    }
+}
