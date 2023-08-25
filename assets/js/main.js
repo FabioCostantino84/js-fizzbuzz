b2f46be851ce42da40fca738264d7b15b842fe2b
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-25 15:50:06 +0200

    add number in console

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* 
+Scrivi un programma che stampi in console i numeri da 1 a 100, ma che:
+per i multipli di 3 stampi “Fizz” al posto del numero
+e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
+
+//stampare in console i numeri da 1 a 100
+let element = document.getElementById('list');
+
+for (let i = 1; i <= 100; i++){
+    console.log(i);
+   
+}
+//stampare in console per i multipli di 3 "Fizz"
+//stampare in console per i multipli di 3 "Fizz"
+//stampare in console sia per i multipli di 3 e di 5 "FizzBuzz"
