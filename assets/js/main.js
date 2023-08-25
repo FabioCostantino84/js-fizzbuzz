cd147fcfa15ac428edc6999f6e393526077de014
FabioCostantino84 <costantinofabio84@gmail.com>
2023-08-25 19:20:27 +0200

    add fixed append

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,27 +10,32 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 //stampare in console per i multipli di 3 "Fizz"
 //stampare in console sia per i multipli di 3 e di 5 "FizzBuzz"
 
+let element = document.getElementById('list');
+
 for (let i = 1; i <= 100; i++) {
+    
+    const liElement = document.createElement('li');
 
     if ((i % 3) === 0 && (i % 5) === 0) {
         console.log("FizzBuzz");
+        liElement.append ('FizzBuzz')
        
     } else if ((i % 3) === 0) {
         console.log("Fizz");
-       
+        liElement.append ('Fizz')
+
     } else if ((i % 5) === 0) {
         console.log("Buzz");
+        liElement.append ('Buzz')
+
        
     } else {
         console.log(i);
+        liElement.append (i);
+
       
     }
-    
-    
-    const liElement = document.createElement('li'); // <li></li>
-    liElement.innerText = i;// <li>1</li>
-    //liElement.classList
 
-    document.getElementById('list').append(liElement); // <li></li>1
+    element.append(liElement);
 
 }
